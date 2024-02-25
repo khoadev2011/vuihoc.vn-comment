ecb2f8b75a1a428808ea21220b28e61272e535eb
khoadev2011 <86996645+khoadev2011@users.noreply.github.com>
2024-02-25 21:17:02 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import sys
+import datetime
+
+logging = False
+
+if logging:
+    sys.stdout = open(f"Log-{datetime.today().strftime('%Y-%m-%d-%H:%M:%S')}.txt")
+current_comment = {}
+room_id = 14685
+while True:
+    comment_trashpile = requests.get(f"https://api.vuihoc.vn/api/comment/live-class/list/{room_id}")
+    comment = json.loads(comment_trashpile.text)
+
+    if current_comment != comment["data"][0]:
+        current_comment = comment["data"][0]
+        print("[" + current_comment["update_date"] + current_comment["user"]["full_name"] + " : " + current_comment["comment"])
